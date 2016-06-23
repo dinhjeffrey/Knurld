db60c8f85374a6ab99cdc538016891d10df04d6e
Andrew <andrew.ying.du@gmail.com>
2016-06-22 18:41:41 -0700

    works in Xcode 7.3.1

--- KnurldTest/ViewController.swift
+++ KnurldTest/ViewController.swift
@@ -26,10 +26,11 @@ class ViewController: UIViewController {
         
         let url = "https://api.knurld.io/oauth/client_credential/accesstoken?grant_type=client_credentials"
         Alamofire.request(.POST, url, parameters: params, headers: headers)
-            .responseJSON { responseRequest, responseResponse, responseResult in
-                print(responseRequest)
-                print(responseResponse)
-                print(responseResult)
+            .responseJSON { response in
+                print(response.request)
+                print(response.response)
+                print(response.data)
+                print(response.result)
 
 
         }
