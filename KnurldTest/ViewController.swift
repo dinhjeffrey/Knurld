1827093fcf31f4a3c9027d0a8f69efe1c22d9140
sarayd <saradubusiness@gmail.com>
2016-06-22 18:00:37 -0700

    first commit

---
+++ KnurldTest/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  KnurldTest
+//
+//  Created by Sara Du on 6/22/16.
+//  Copyright © 2016 Sara Du. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    var json = JSON([])
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let params = [
+            "client_id": "D7cAa01QbDCjHKjMQHvoi5GMJyhlPrGy",
+            "client_secret": "KPhSuyMrV9pYqPIz"
+        ]
+        
+        let headers = [
+             "Content-Type" : "application/x-www-form-urlencoded"
+        ]
+        
+        let url = "https://api.knurld.io/oauth/client_credential/accesstoken?grant_type=client_credentials"
+        Alamofire.request(.POST, "https://httpbin.org/get", headers: headers)
+            .responseJSON { response in
+                debugPrint(response)
+        }
+        
+        Alamofire.request(.GET, url, parameters: params, encoding: ParameterEncoding.URL).responseJSON { (_, _, result) in
+            switch result {
+            case .Success(let data):
+                self.json = JSON(data)
+                self.tableview.reloadData()
+            case .Failure(_, let error):
+                print("Request failed with error: \(error)")
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-KnurldTest/Pods-KnurldTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KnurldTest : NSObject
+@end
+@implementation PodsDummy_Pods_KnurldTest
+@end
