9d415f32934b58a3442dce9a225be3921d2fb65e
sarayd <saradubusiness@gmail.com>
2016-06-24 10:14:37 -0700

    create user

--- KnurldTest/ViewController.swift
+++ KnurldTest/ViewController.swift
@@ -34,6 +34,26 @@ class ViewController: UIViewController {
         }
     }
 
+    func createUser(name: String, gender: String, password: String){
+        let url = "https://api.knurld.io/v1/consumers"
+        let params = [
+            "username": name,
+            "gender": gender,
+            "password": password
+        ]
+        
+        let headers = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + accessToken,
+            "Developer-Id" : "Bearer: eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MDQ4MTY5MDUsInJvbGUiOiJhZG1pbiIsImlkIjoiZWNkMTAwM2YzODJlNWEzZjU0NGQyZjFkY2Y3YmNmYjUiLCJ0ZW5hbnQiOiJ0ZW5hbnRfbXJwdGF4M2xuenl4cXpsem5qeHhhenR2bzQyaHU2dHBudnpkZTVsYnBpenc0M2xnb3YzeHMzZHVtcnhkazUzciIsIm5hbWUiOiJhZG1pbiJ9.El88CANBe5C_KLpYlP7dc-5-dwF-zPFGk2YeubNobm59uM2Sx9NbVGcN5n7smm4izo1s0RsrVKHBd9mH4hkPQA"
+        ]
+
+        Alamofire.request(.POST, url, parameters: params, headers: headers)
+            .responseJSON { response in
+                
+        }
+
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
