7b9421423f8636c5f619635de085ddaed28bbd5a
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-06-24 15:15:24 -0700

    added createEnrollment

--- KnurldTest/ViewController.swift
+++ KnurldTest/ViewController.swift
@@ -12,6 +12,7 @@ import Alamofire
 class ViewController: UIViewController {
     var json = JSON([])
     var accessToken = ""
+    var enrollmentID = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,6 +58,32 @@ class ViewController: UIViewController {
         }
     }
     
+    func createEnrollment(consumer: String, application: String) {
+        let url = "https://api.knurld.io/v1/enrollments/"
+        let params = [
+            "consumer": consumer,
+            "application": application
+        ]
+        
+        let headers = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + accessToken,
+            "Developer-Id" : "Bearer: eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MDQ4MTY5MDUsInJvbGUiOiJhZG1pbiIsImlkIjoiZWNkMTAwM2YzODJlNWEzZjU0NGQyZjFkY2Y3YmNmYjUiLCJ0ZW5hbnQiOiJ0ZW5hbnRfbXJwdGF4M2xuenl4cXpsem5qeHhhenR2bzQyaHU2dHBudnpkZTVsYnBpenc0M2xnb3YzeHMzZHVtcnhkazUzciIsIm5hbWUiOiJhZG1pbiJ9.El88CANBe5C_KLpYlP7dc-5-dwF-zPFGk2YeubNobm59uM2Sx9NbVGcN5n7smm4izo1s0RsrVKHBd9mH4hkPQA"
+        ]
+        
+        var request = NSMutableURLRequest(URL: NSURL(fileURLWithPath: url))
+        let encoding = Alamofire.ParameterEncoding.URL
+        (request, _) = encoding.encode(request, parameters: params)
+        
+        
+        Alamofire.request(.POST, url, parameters: params, headers: headers, encoding: .JSON)
+            .responseJSON { response in
+                if let enrollmentID = response.result.value?["href"] {
+                    self.enrollmentID = enrollmentID as! String
+                }
+        }
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
