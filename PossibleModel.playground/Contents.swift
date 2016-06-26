bff5340c3a971df617a614e1a1821dc4f1928354
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-06-26 09:40:57 -0700

    added possible model playground

---
+++ PossibleModel.playground/Contents.swift
@@ -0,0 +1,57 @@
+//: Playground - noun: a place where people can play
+// possible model structure?
+import UIKit
+import XCPlayground
+
+struct AppModelRequet {
+    var repeats: Int
+    var vocabulary: [String] = []
+    var verificationLength: Int
+}
+
+struct AppModel {
+    var url: NSURL
+}
+
+enum Gender: CustomStringConvertible {
+    case male, female
+    
+    var description: String {
+        switch self {
+        case .male:
+            return "M"
+        case .female:
+            return "F"
+        }
+    }
+}
+
+struct Consumer {
+    var gender: Gender
+    var username: String
+    var password: String
+    var url: NSURL?
+}
+
+struct Enrollment {
+    enum Status {
+        case initialized, requested, processing, completed, failed, removedRequested, removing
+    }
+    var consumer: Consumer
+    var application: AppModel
+    var status: Status
+}
+
+typealias TimePosition = Int
+struct Phrase {
+    var name: String
+    var start: TimePosition
+    var stop: TimePosition
+}
+struct AudioFile {
+    var url: NSURL
+}
+struct Analysis {
+    var audioUrl: NSURL
+    var filedata: NSFileManager
+}
