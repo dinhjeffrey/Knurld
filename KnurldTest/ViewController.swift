6736574d90ed03463ba554a5db217d193a1600c2
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-06-27 16:36:28 -0700

    updated populateEnrollment

--- KnurldTest/ViewController.swift
+++ KnurldTest/ViewController.swift
@@ -83,63 +83,27 @@ class ViewController: UIViewController {
                 }
         }
     }
+    typealias TimePosition = Int
+    struct Interval {
+        var phrase: String
+        var start: TimePosition
+        var stop: TimePosition
+    }
     
-    func populateEnrollment(audioLink: String,
-                            phrase1: String, start1: Int, stop1: Int, start2: Int, stop2: Int, start3: Int, stop3: Int,
-                            phrase2: String, start4: Int, stop4: Int, start5: Int, stop5: Int, start6: Int, stop6: Int,
-                            phrase3: String, start7: Int, stop7: Int, start8: Int, stop8: Int, start9: Int, stop9: Int ) {
+    func populateEnrollment(audioLink: NSURL,
+                            intervals: [Interval] ) {
         let url = enrollmentID
-        let intervals = [
+        let intervalsDictionary = [
             [
-                "phrase": phrase1,
-                "start": start1,
-                "stop": stop1,
-            ],
-            [
-                "phrase": phrase1,
-                "start": start2,
-                "stop": stop2,
-            ],
-            [
-                "phrase": phrase1,
-                "start": start3,
-                "stop": stop3,
-            ],
-            [
-                "phrase": phrase2,
-                "start": start4,
-                "stop": stop4,
-            ],
-            [
-                "phrase": phrase2,
-                "start": start5,
-                "stop": stop5,
-            ],
-            [
-                "phrase": phrase2,
-                "start": start6,
-                "stop": stop6,
-            ],
-            [
-                "phrase": phrase3,
-                "start": start7,
-                "stop": stop7,
-            ],
-            [
-                "phrase": phrase3,
-                "start": start8,
-                "stop": stop8,
-            ],
-            [
-                "phrase": phrase3,
-                "start": start9,
-                "stop": stop9
+                "phrase": intervals[0],
+                "start": intervals[1],
+                "stop": intervals[2],
             ]
         ]
         
-        let params = [
+        let params : [String: AnyObject] = [
             "enrollment.wav": audioLink,
-            "intervals": intervals
+            "intervals": (intervalsDictionary as? AnyObject)!
         ]
         
         let headers = [
@@ -150,9 +114,9 @@ class ViewController: UIViewController {
         
         var request = NSMutableURLRequest(URL: NSURL(fileURLWithPath: url))
         let encoding = Alamofire.ParameterEncoding.URL
-        (request, _) = encoding.encode(request, parameters: params as? [String : AnyObject])
+        (request, _) = encoding.encode(request, parameters: params)
         
-        Alamofire.request(.POST, url, parameters: params as? [String : AnyObject], headers: headers, encoding: .JSON)
+        Alamofire.request(.POST, url, parameters: params, headers: headers, encoding: .JSON)
             .responseJSON { response in
                 print(response)
         }
